5322bd0cc710248dc90e9849db97ecbb9cd66d84
Aanchal Shah <aanchalshah2018@gmail.com>
2026-03-28 13:55:36 +0530

    Fix nested repo issue

---
+++ Documents/grocify/grocifyapp/app/index.tsx
@@ -0,0 +1,17 @@
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { Image } from "expo-image";
+export default function Index() {
+  return (
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >/
+      {/* <Text>Hello</Text>
+      <Pressable onPress={() => console.log("Pressed!")}></Pressable>
+      <Image source={require("../assets/images/icon.png")} style={{ width: 200, height: 200 }} />
+      <TouchableOpacity></TouchableOpacity> */}
+    </View>
+  );
+}
